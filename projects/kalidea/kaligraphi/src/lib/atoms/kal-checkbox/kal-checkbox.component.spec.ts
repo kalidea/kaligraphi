bd970bce05282fc2ccf6280187de01e2f0e31811
mgrosio <mgrosio@kalidea.com>
2018-10-18 14:51:49 +0200

    kal-checkbox (TEST) : Should contains a checkbox element

--- projects/kalidea/kaligraphi/src/lib/atoms/kal-checkbox/kal-checkbox.component.spec.ts
+++ projects/kalidea/kaligraphi/src/lib/atoms/kal-checkbox/kal-checkbox.component.spec.ts
@@ -1,25 +1,39 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ElementRef, NO_ERRORS_SCHEMA } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 import { KalCheckboxComponent } from './kal-checkbox.component';
 
 describe('KalCheckboxComponent', () => {
   let component: KalCheckboxComponent;
   let fixture: ComponentFixture<KalCheckboxComponent>;
+  let elementRef: ElementRef;
+  let checkbox;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ KalCheckboxComponent ]
+      declarations: [
+        KalCheckboxComponent,
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
     })
-    .compileComponents();
+      .createComponent(KalCheckboxComponent);
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(KalCheckboxComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    elementRef = fixture.debugElement.injector.get(ElementRef);
+    checkbox = fixture.debugElement.queryAll(By.css('input[type=checkbox]'));
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should contains checbox element', () => {
+    expect(checkbox.length).toEqual(1);
   });
 });
