721f4b2814b5f3860ad31405cb4c5739f2def9fe
mgrosio <mgrosio@kalidea.com>
2018-10-18 15:35:32 +0200

    kal-checkbox (TEST) : Creation of all unit test

--- projects/kalidea/kaligraphi/src/lib/atoms/kal-checkbox/kal-checkbox.component.spec.ts
+++ projects/kalidea/kaligraphi/src/lib/atoms/kal-checkbox/kal-checkbox.component.spec.ts
@@ -33,7 +33,45 @@ describe('KalCheckboxComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should contains checbox element', () => {
+  it('should contains checkbox element', () => {
     expect(checkbox.length).toEqual(1);
+  });
+
+  it('should have a form control with a true value', () => {
+    expect(component.control.value).toBeFalsy();
+
+    checkbox[0].triggerEventHandler('click', null);
+
+    expect(component.control.value).toBeTruthy();
+  });
+
+  it('should have the formControl value to true', () => {
+    component.value = false;
+    component.ngOnInit();
+
+    expect(component.control.value).toBeFalsy();
+
+    component.value = true;
+    component.ngOnInit();
+
+    expect(component.control.value).toBeTruthy();
+  });
+
+  it('should emit an event with the form control value when the value changes', () => {
+    spyOn(component, 'notifyUpdate');
+    spyOn(component.toggled, 'emit');
+
+    component.control.patchValue(true);
+
+    expect(component.notifyUpdate).toHaveBeenCalledWith(true);
+    expect(component.toggled.emit).toHaveBeenCalledWith(true);
+  });
+
+  it('should update the form control value when a new value is set', () => {
+    expect(component.control.value).toBeFalsy();
+
+    component.writeValue(true);
+
+    expect(component.control.value).toBeTruthy();
   });
 });
--- projects/kalidea/kaligraphi/src/lib/atoms/kal-checkbox/kal-checkbox.component.ts
+++ projects/kalidea/kaligraphi/src/lib/atoms/kal-checkbox/kal-checkbox.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, OnInit, Output, ViewEncapsulation } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { buildProviders, FormElementComponent } from '../../utils/index';
 
 @Component({
@@ -10,6 +11,16 @@ import { buildProviders, FormElementComponent } from '../../utils/index';
   providers: buildProviders(KalCheckboxComponent)
 })
 export class KalCheckboxComponent extends FormElementComponent<boolean> implements OnInit {
+
+  /**
+   * The form control that contains the checkbox value
+   */
+  control = new FormControl(false);
+
+  /**
+   * Triggered when the checkbox value has changed
+   */
+  @Output() toggled = new EventEmitter<boolean>();
 
   constructor() {
     super();
