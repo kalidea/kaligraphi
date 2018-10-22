8118c6e67395db8d560dcb42b215183bb1a42e88
jsp-up <44195034+jsp-up@users.noreply.github.com>
2018-10-22 14:55:29 +0200

    Feature/rater (#3)
    * KalIcon
    
    * KalIcon
    
    * KalRater - README
    
    * KalRater - END
    
    * KalIcon - Add playground
    
    * KalRater - Fix #1
    
    * KalRater - Add playground example
    
    * KalRater
    
    * Fix imports
    
    * Fix build
    
    * KalRater - Fix tests
    
    * Add ignore files

---
+++ projects/kalidea/kaligraphi/src/lib/molecules/kal-rater/kal-rater.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { KalRaterComponent } from './kal-rater.component';
+import { KalIconModule } from '../../atoms/kal-icon/kal-icon.module';
+export { KalRaterComponent } from './kal-rater.component';
+
+const exports = [
+  KalRaterComponent
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    KalIconModule
+  ],
+  exports: exports,
+  declarations: exports
+})
+export class KalRaterModule { }
