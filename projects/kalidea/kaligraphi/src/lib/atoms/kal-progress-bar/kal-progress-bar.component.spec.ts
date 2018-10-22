e40a59e97b602709137bd097a5a9e40aa4311842
Julien <jspaeter@kalidea.com>
2018-10-22 11:10:49 +0200

    KalProgressBar - Start

--- projects/kalidea/kaligraphi/src/lib/atoms/kal-progress-bar/kal-progress-bar.component.spec.ts
+++ projects/kalidea/kaligraphi/src/lib/atoms/kal-progress-bar/kal-progress-bar.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { KalProgressBarComponent } from './kal-progress-bar.component';
 
@@ -8,18 +9,42 @@ describe('KalProgressBarComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ KalProgressBarComponent ]
+      declarations: [KalProgressBarComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(KalProgressBarComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should change the progress-bar color according to a CSS class', () => {
+
+  });
+
+  it('should have a default progress value', () => {
+    const progressBar = fixture.debugElement.query(By.css('div'));
+    component.value = 40;
+
+    // update view
+    fixture.detectChanges();
+
+    expect(progressBar.styles.width).toEqual(component.value + '%');
+  });
+
+  it('should clamp value between 0 and 100', () => {
+    component.value = 50;
+    expect(component.value).toBe(50);
+
+    component.value = 999;
+    expect(component.value).toBe(100);
+
+    component.value = -10;
+    expect(component.value).toBe(0);
   });
 });
--- projects/kalidea/kaligraphi/src/lib/atoms/kal-progress-bar/kal-progress-bar.component.ts
+++ projects/kalidea/kaligraphi/src/lib/atoms/kal-progress-bar/kal-progress-bar.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+  ViewEncapsulation
+} from '@angular/core';
+import { coerceNumberProperty } from '@angular/cdk/coercion';
+import { clamp } from '../../utils/helpers/numbers';
 
 @Component({
   selector: 'kal-progress-bar',
@@ -7,11 +18,38 @@ import { Component, OnInit, ViewEncapsulation, ChangeDetectionStrategy } from '@
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class KalProgressBarComponent implements OnInit {
+export class KalProgressBarComponent implements OnInit, OnChanges {
 
-  constructor() { }
+  /**
+   * Progress bar default value
+   */
+  private progressValue = 0;
+
+  constructor(private cdr: ChangeDetectorRef) {
+  }
+
+  /**
+   * Progress-bar color
+   */
+  @Input() color: string;
+
+  /**
+   * Value of the progress-bar. Defaults to zero. Max to 100
+   */
+  @Input()
+  get value(): number {
+    return this.progressValue;
+  }
+  set value(newValue: number) {
+    this.progressValue = clamp(coerceNumberProperty(newValue));
+  }
 
   ngOnInit() {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    // update view
+    this.cdr.markForCheck();
   }
 
 }
