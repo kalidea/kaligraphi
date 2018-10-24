7aa1d4ccaec6d76135dc5b0041a0d316adbc98f1
Julien <jspaeter@kalidea.com>
2018-10-24 16:10:35 +0200

    KalAccordion : Panel Content

--- projects/kalidea/kaligraphi/src/lib/molecules/kal-accordion/kal-accordion.module.ts
+++ projects/kalidea/kaligraphi/src/lib/molecules/kal-accordion/kal-accordion.module.ts
@@ -8,6 +8,11 @@ export { KalExpansionPanelComponent } from './kal-expansion-panel/kal-expansion-
 // Panel : Header
 import { KalExpansionPanelHeaderComponent } from './kal-expansion-panel-header/kal-expansion-panel-header.component';
 export { KalExpansionPanelHeaderComponent } from './kal-expansion-panel-header/kal-expansion-panel-header.component';
+
+// Panel : Content
+import { KalExpansionPanelContentComponent } from './kal-expansion-panel-content/kal-expansion-panel-content.component';
+export { KalExpansionPanelContentComponent } from './kal-expansion-panel-content/kal-expansion-panel-content.component';
+
 // Panel : Action Row
 import { KalActionRowComponent } from './kal-action-row/kal-action-row.component';
 export { KalActionRowComponent } from './kal-action-row/kal-action-row.component';
@@ -15,6 +20,7 @@ export { KalActionRowComponent } from './kal-action-row/kal-action-row.component
 const exports = [
   KalExpansionPanelComponent,
   KalExpansionPanelHeaderComponent,
+  KalExpansionPanelContentComponent,
   KalActionRowComponent
 ];
 @NgModule({
