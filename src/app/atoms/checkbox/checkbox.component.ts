64b9cc3f5675f8a3cae31d9ff11b7d0be6ab95f4
MichaelGrosio <michael.grosio@outlook.fr>
2018-10-24 10:08:50 +0200

    Feature/kal checkbox (#1)
    * kal-checkbox (TEST) : Should contains a checkbox element
    
    * kal-checkbox (TEST) : Creation of all unit test
    
    * kal-checkbox : Creation of all methods of the checkbox component
    
    * kal-checkbox (TEST): Correction of unit test
    
    * kal-checkbox : Correction of checkbox component
    
    * kal-checkbox : Correction of checkbox component
    
    * kal-checkbox : Correction of checkbox component
    
    * kal-checkbox : Correction of karma.conf
    
    * kal-checkbox : Correction PR

--- src/app/atoms/checkbox/checkbox.component.ts
+++ src/app/atoms/checkbox/checkbox.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-checkbox',
@@ -9,7 +10,62 @@ import { Component, OnInit, ViewEncapsulation, ChangeDetectionStrategy } from '@
 })
 export class CheckboxComponent implements OnInit {
 
-  constructor() { }
+  /**
+   * control that contains the checkbox value
+   */
+  control = new FormControl(true);
+
+  /**
+   * The disabled state of the checkbox
+   */
+  disabled = false;
+
+  /**
+   * Value retrieved by checkbox output
+   */
+  private value = true;
+
+  constructor() {
+  }
+
+  /**
+   * Return checkbox value by using control
+   */
+  get valueByControl(): boolean {
+    return this.control.value;
+  }
+
+  /**
+   * Return checkbox value by using output
+   */
+  get valueByOutput(): boolean {
+    return this.value;
+  }
+
+  /**
+   * Set the checkbox value
+   */
+  setValue(value) {
+    this.value = value;
+  }
+
+  /**
+   * Toggle the disabled state of the checkbox by using control
+   */
+  toggleDisableStateByControl() {
+    if (this.control.disabled) {
+      this.control.enable();
+    } else {
+      this.control.disable();
+    }
+  }
+
+  /**
+   * Toggle disabled state of the checkbox with input
+   */
+  toggleDisableStateByInput() {
+    this.disabled = !this.disabled;
+  }
 
   ngOnInit() {
   }
