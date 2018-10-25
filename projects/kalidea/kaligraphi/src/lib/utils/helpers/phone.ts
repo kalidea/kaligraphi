0093d36afc6d458669d2b243425e8caed68a8c34
ydomenjoud <yannick@6tech.net>
2018-10-25 11:30:38 +0200

    fix code climate issue

--- projects/kalidea/kaligraphi/src/lib/utils/helpers/phone.ts
+++ projects/kalidea/kaligraphi/src/lib/utils/helpers/phone.ts
@@ -17,24 +17,25 @@ export function formatPhoneNumber(phoneNumber: string): string {
 }
 
 /**
- * helper function to build formatter
- */
-function buildFormatterFunction(positionMin, modulo, indexes = []): (i: number) => boolean {
-  return (i) => indexes.indexOf(i) >= 0 || (i >= positionMin && i % 2 === modulo);
-}
-
-/**
  * get phone formatter for the given value
  */
 function getPhoneFormatter(value): (i: number) => boolean {
 
+  let positionMin = 0;
+  let modulo = 1;
+  let indexes = [];
+
   if (value.substring(0, 2) === '00') {
-    return buildFormatterFunction(6, 1, [4, 5]);
+    positionMin = 6;
+    modulo = 1;
+    indexes = [4, 5];
   } else if (value.charAt(0) === '+') {
-    return buildFormatterFunction(5, 0, [3, 4]);
+    positionMin = 5;
+    modulo = 0;
+    indexes = [3, 4];
   }
 
-  return buildFormatterFunction(1, 0);
+  return (i) => indexes.indexOf(i) >= 0 || (i >= positionMin && i % 2 === modulo);
 
 }
 
