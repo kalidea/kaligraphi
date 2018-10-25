abb43dfe49d90c4a3a34d05f871a729008fb5b66
ydomenjoud <yannick@6tech.net>
2018-10-25 11:10:55 +0200

    fix similar block

--- projects/kalidea/kaligraphi/src/lib/utils/helpers/phone.ts
+++ projects/kalidea/kaligraphi/src/lib/utils/helpers/phone.ts
@@ -1,22 +1,42 @@
 /**
  * Format phone number
  */
+
+enum PhoneFormatter {
+  Double0 = '00',
+  Plus = '+',
+  Default = 'default'
+}
+
 export function formatPhoneNumber(phoneNumber: string): string {
   let formatted: string[] = [];
 
   if (phoneNumber) {
     const value = phoneNumber.replace(/\s/g, '');
 
-    if (value.substring(0, 2) === '00') {
-      formatted = formatPhoneNumberWithSpaces(value, (i) => (i >= 4 && i <= 5) || (i >= 6 && i % 2 === 1));
-    } else if (value.charAt(0) === '+') {
-      formatted = formatPhoneNumberWithSpaces(value, (i) => (i >= 3 && i <= 4) || (i >= 5 && i % 2 === 0));
-    } else {
-      formatted = formatPhoneNumberWithSpaces(value, (i) => i % 2 === 0 && i > 0);
-    }
+    formatted = formatPhoneNumberWithSpaces(value, getPhoneFormatter(value));
+
   }
 
   return formatted.join('');
+}
+
+function getPhoneFormatter(value): (number) => boolean {
+
+  let formatter = PhoneFormatter.Default;
+  if (value.substring(0, 2) === '00') {
+    formatter = PhoneFormatter.Double0;
+  } else if (value.charAt(0) === '+') {
+    formatter = PhoneFormatter.Plus;
+  }
+
+  const phoneFormatter = {
+    [PhoneFormatter.Double0]: (i) => (i >= 4 && i <= 5) || (i >= 6 && i % 2 === 1),
+    [PhoneFormatter.Plus]: (i) => (i >= 3 && i <= 4) || (i >= 5 && i % 2 === 0),
+    [PhoneFormatter.Default]: (i) => i > 0 && i % 2 === 0
+  };
+
+  return phoneFormatter[formatter];
 }
 
 function formatPhoneNumberWithSpaces(value, includeSpace: (i: number) => boolean): string[] {
