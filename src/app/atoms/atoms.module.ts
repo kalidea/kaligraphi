c735bf566ce041fc297e1f98c9acbd4367ed426b
MichaelGrosio <michael.grosio@outlook.fr>
2018-10-29 16:44:29 +0100

    Kal radio (#7)
    add  kal-radio feature

--- src/app/atoms/atoms.module.ts
+++ src/app/atoms/atoms.module.ts
@@ -7,6 +7,7 @@ import { AtomsRoutingModule } from 'src/app/atoms/atoms-routing.module';
 import { InputComponent } from 'src/app/atoms/input/input.component';
 import { SelectComponent } from 'src/app/atoms/select/select.component';
 import { CheckboxComponent } from 'src/app/atoms/checkbox/checkbox.component';
+import { RadioComponent } from 'src/app/atoms/radio/radio.component';
 import { IconComponent } from 'src/app/atoms/icon/icon.component';
 import { RaterComponent } from 'src/app/atoms/rater/rater.component';
 import { ProgressBarComponent } from 'src/app/atoms/progress-bar/progress-bar.component';
@@ -23,6 +24,7 @@ import { ProgressBarComponent } from 'src/app/atoms/progress-bar/progress-bar.co
     InputComponent,
     SelectComponent,
     CheckboxComponent,
+    RadioComponent,
     IconComponent,
     RaterComponent,
     ProgressBarComponent,
---
+++ src/app/atoms/radio/radio.component.ts
@@ -0,0 +1,90 @@
+import { ChangeDetectionStrategy, Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { KalRadioChange } from '@kalidea/kaligraphi';
+
+@Component({
+  selector: 'app-radio',
+  templateUrl: './radio.component.html',
+  styleUrls: ['./radio.component.sass'],
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class RadioComponent implements OnInit {
+
+  /**
+   * Value of radio button group
+   */
+  value = 'test';
+
+  /**
+   * The form control of the radio button group
+   */
+  control = new FormControl(this.value);
+
+  /**
+   * The name of radio button group by using control
+   */
+  nameRadioButtonWithControl = 'radioButtonWithControl';
+
+  /**
+   * The name of radio button group by using event
+   */
+  nameRadioButtonWithEvent = 'radioButtonWithEvent';
+
+  /**
+   * Is radio button group disabled
+   */
+  disabled = false;
+
+  /**
+   * Id of selected radio button
+   */
+  id: string;
+
+  constructor() {
+  }
+
+  /**
+   * Change radio button group value by using control
+   */
+  changeRadioValueWithControl(value: string) {
+    this.control.patchValue(value);
+  }
+
+  /**
+   * Change radio button group value by using input
+   */
+  changeRadioValueWithEvent(value: string) {
+    this.value = value;
+  }
+
+  /**
+   * Display the selected radio button id and its value
+   */
+  displayValue($event: KalRadioChange) {
+    this.id = $event.source.id;
+    this.value = $event.value;
+  }
+
+  /**
+   * Disabled the radio button group that using control
+   */
+  disableGroupByControl() {
+    if (!this.control.disabled) {
+      this.control.disable();
+    } else {
+      this.control.enable();
+    }
+  }
+
+  /**
+   * Disabled the radio button group that using input
+   */
+  disableGroupByInput() {
+    this.disabled = !this.disabled;
+  }
+
+  ngOnInit() {
+  }
+
+}
