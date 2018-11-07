980e2faf228796beefe0792b1a619f2736fdc378
Franck Martin <franck.martin@up.coop>
2018-11-07 12:22:29 +0100

    Fix ValueChange FormControl

--- projects/kalidea/kaligraphi/src/lib/molecules/kal-select/kal-select.component.ts
+++ projects/kalidea/kaligraphi/src/lib/molecules/kal-select/kal-select.component.ts
@@ -184,11 +184,11 @@ export class KalSelectComponent
   /**
    * Select an option by his value
    */
-  select(value: any): void {
+  select(value: any, notify = false): void {
     const optionSelect = this.options.find((item) => item.value === value);
     if (optionSelect) {
       this.keyManager.setActiveItem(optionSelect);
-      this.optionSelected(this.keyManager.activeItem);
+      this.optionSelected(this.keyManager.activeItem, notify);
     }
   }
 
@@ -261,15 +261,19 @@ export class KalSelectComponent
    * Event emitted when an option is selected
    * Set the option as active
    * @param option KalOptionComponent
+   * @param withNotify boolean
    */
-  private optionSelected(option: KalOptionComponent) {
+  private optionSelected(option: KalOptionComponent, withNotify: boolean = true) {
     if (this.multiple) {
       this.optionSelectedOnMultipleMode(option);
     } else {
       this.optionSelectedOnSimpleMode(option);
     }
 
-    super.notifyUpdate(this.selectedValue);
+    if (withNotify) {
+      super.notifyUpdate(this.selectedValue);
+    }
+
     this.valueChange.emit(this.selectedValue);
     this.cdr.markForCheck();
   }
