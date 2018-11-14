4f3549f8b1ff1da5dd068e57e7bfbc0453333e8a
Jérôme POË <jpoe@kalidea.com>
2018-11-14 19:30:52 +0100

    Review kal-button

--- src/app/molecules/button/button.component.ts
+++ src/app/molecules/button/button.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { KalRadioChange } from '@kalidea/kaligraphi';
 
 @Component({
   selector: 'app-button',
@@ -13,7 +15,46 @@ export class ButtonComponent implements OnInit {
 
   content = 'libellé';
 
+  reversed = false;
+
+  large = false;
+
+  nivo = 'default';
+
+  /**
+   * The form control of the radio button group
+   */
+  control = new FormControl(this.nivo);
+
+  /**
+   * The name of radio button group by using event
+   */
+  nameButtonLevel = 'buttonLevel';
+
   constructor() { }
+
+  reversedButton() {
+    if (this.reversed) {
+      this.reversed = !this.reversed;
+    } else {
+      this.reversed = true;
+    }
+  }
+
+  enlargedButton() {
+    if (this.large) {
+      this.large = !this.large;
+    } else {
+      this.large = true;
+    }
+  }
+
+  /**
+   * Display the selected radio button id and its value
+   */
+  displayValue($event: KalRadioChange) {
+    this.nivo = $event.value;
+  }
 
   ngOnInit() {
   }
