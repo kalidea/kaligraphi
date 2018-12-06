598cee9b45122fe8ce3fd66c1806f5d0b9000c7c
mgrosio <mgrosio@kalidea.com>
2018-12-06 11:45:02 +0100

    kal-text-area : Correction of PR

--- projects/kalidea/kaligraphi/src/lib/atoms/kal-atoms.module.ts
+++ projects/kalidea/kaligraphi/src/lib/atoms/kal-atoms.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { KalInputModule } from './kal-input/kal-input.module';
+import { KalTextareaModule } from './kal-textarea/kal-textarea.module';
 import { KalCheckboxModule } from './kal-checkbox/kal-checkbox.module';
 import { KalRadioModule } from './kal-radio/kal-radio.module';
 import { KalOptionModule } from './kal-option/kal-option.module';
@@ -11,6 +12,7 @@ import { KalSliderModule } from './kal-slider/kal-slider.module';
 import { KalMenuModule } from './kal-menu/kal-menu.module';
 
 export * from './kal-input/kal-input.module';
+export * from './kal-textarea/kal-textarea.module';
 export * from './kal-checkbox/kal-checkbox.module';
 export * from './kal-radio/kal-radio.module';
 export * from './kal-icon/kal-icon.module';
@@ -21,6 +23,7 @@ export * from './kal-menu/kal-menu.module';
 
 const exports = [
   KalInputModule,
+  KalTextareaModule,
   KalCheckboxModule,
   KalRadioModule,
   KalIconModule,
--- projects/kalidea/kaligraphi/src/lib/atoms/kal-textarea/kal-textarea.component.spec.ts
+++ projects/kalidea/kaligraphi/src/lib/atoms/kal-textarea/kal-textarea.component.spec.ts
@@ -1,7 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { KalTextareaComponent } from './kal-textarea.component';
-import { ReactiveFormsModule } from '@angular/forms';
 
 describe('KalTextareaComponent', () => {
   let component: KalTextareaComponent;
