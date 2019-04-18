078db7824d36a1342665afd0012fcf9e1905a734
mgrosio <mgrosio@kalidea.com>
2019-04-18 12:30:16 +0200

    Kal List Add ngClass to kal list item

--- projects/kalidea/kaligraphi/src/lib/03-layout/kal-list/kal-list-item.directive.ts
+++ projects/kalidea/kaligraphi/src/lib/03-layout/kal-list/kal-list-item.directive.ts
@@ -1,9 +1,14 @@
-import { Directive, TemplateRef } from '@angular/core';
+import { Directive, Input, TemplateRef } from '@angular/core';
 
 @Directive({
   selector: '[kalListItem]'
 })
 export class KalListItemDirective {
+
+  /**
+   * Add class to kal list item
+   */
+  @Input() ngClass: any;
 
   constructor(public view: TemplateRef<any>) {
   }
