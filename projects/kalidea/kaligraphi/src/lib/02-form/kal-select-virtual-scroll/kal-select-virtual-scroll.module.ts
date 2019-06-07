6eb5844f0d92fab16a81ea26f6921e59b5a3a185
cdavidenko <corentin.davidenko@up.coop>
2019-06-07 11:27:04 +0200

    réorganisation des imports

--- projects/kalidea/kaligraphi/src/lib/02-form/kal-select-virtual-scroll/kal-select-virtual-scroll.module.ts
+++ projects/kalidea/kaligraphi/src/lib/02-form/kal-select-virtual-scroll/kal-select-virtual-scroll.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
-import { ScrollingModule } from '@angular/cdk/scrolling';
-import { KalSelectVirtualScrollComponent } from './kal-select-virtual-scroll.component';
 import { OverlayModule } from '@angular/cdk/overlay';
 import { PortalModule } from '@angular/cdk/portal';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+
+import { KalSelectVirtualScrollComponent } from './kal-select-virtual-scroll.component';
 
 const exports = [
   KalSelectVirtualScrollComponent
