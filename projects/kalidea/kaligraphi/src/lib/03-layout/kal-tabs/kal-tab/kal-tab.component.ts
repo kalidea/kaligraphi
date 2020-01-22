9a5c46664190a4773c27794f22ef279295679cf0
michaelgrosio <michael.grosio@outlook.fr>
2020-01-22 16:00:41 +0100

    Fix ka-tab : Add templateRef

--- projects/kalidea/kaligraphi/src/lib/03-layout/kal-tabs/kal-tab/kal-tab.component.ts
+++ projects/kalidea/kaligraphi/src/lib/03-layout/kal-tabs/kal-tab/kal-tab.component.ts
@@ -5,8 +5,11 @@ import {
   forwardRef,
   Inject,
   Input,
+  OnInit,
   Optional,
+  TemplateRef,
   ViewChild,
+  ViewContainerRef,
   ViewEncapsulation
 } from '@angular/core';
 import { TemplatePortal } from '@angular/cdk/portal';
@@ -14,11 +17,12 @@ import { coerceBooleanProperty } from '@angular/cdk/coercion';
 
 import { KalTabLabelDirective } from '../kal-tab-label.directive';
 import { KalTabGroupComponent } from '../kal-tab-group/kal-tab-group.component';
+import { KalTabContentDirective } from '../kal-tab-content.directive';
 
 @Component({
   selector: 'kal-tab',
   template: `
-    <ng-template cdkPortal #tabContent="cdkPortal">
+    <ng-template>
       <ng-content>
       </ng-content>
     </ng-template>
@@ -26,7 +30,37 @@ import { KalTabGroupComponent } from '../kal-tab-group/kal-tab-group.component';
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class KalTabComponent {
+export class KalTabComponent implements OnInit {
+
+  /**
+   * Template label of the header
+   */
+  @ContentChild(KalTabLabelDirective, {static: true}) templateLabel: KalTabLabelDirective;
+  /**
+   * Template provided in the tab content that will be used if present, used to enable lazy-loading
+   */
+  @ContentChild(KalTabContentDirective, {read: TemplateRef, static: true}) _explicitContent: TemplateRef<any>;
+  /**
+   * Template inside the MatTab view that contains an `<ng-content>`.
+   */
+  @ViewChild(TemplateRef, {static: true}) _implicitContent: TemplateRef<any>;
+  /**
+   * Is a tab selected
+   */
+  private selectedTab = false;
+  /**
+   * Is a tab disabled
+   */
+  private isDisabled = false;
+  /**
+   * Label of the header
+   */
+  private tabLabel = '';
+  private _contentPortal: TemplatePortal | null = null;
+
+  constructor(@Optional() @Inject(forwardRef(() => KalTabGroupComponent)) public tabGroup: KalTabGroupComponent,
+              private _viewContainerRef: ViewContainerRef) {
+  }
 
   /**
    * Label of the header
@@ -41,34 +75,6 @@ export class KalTabComponent {
     if (this.tabGroup) {
       this.tabGroup.markForTabGroupCheck();
     }
-  }
-
-  /**
-   * Template label of the header
-   */
-  @ContentChild(KalTabLabelDirective, {static: true}) templateLabel: KalTabLabelDirective;
-
-  /**
-   * The reference to the template portal
-   */
-  @ViewChild('tabContent', {static: true}) tabContent: TemplatePortal<any>;
-
-  /**
-   * Is a tab selected
-   */
-  private selectedTab = false;
-
-  /**
-   * Is a tab disabled
-   */
-  private isDisabled = false;
-
-  /**
-   * Label of the header
-   */
-  private tabLabel = '';
-
-  constructor(@Optional() @Inject(forwardRef(() => KalTabGroupComponent)) public tabGroup: KalTabGroupComponent) {
   }
 
   /**
@@ -99,6 +105,11 @@ export class KalTabComponent {
    * Return the content of the tab
    */
   get content(): TemplatePortal<any> {
-    return this.tabContent;
+    return this._contentPortal;
+  }
+
+  ngOnInit(): void {
+    this._contentPortal = new TemplatePortal(
+      this._explicitContent || this._implicitContent, this._viewContainerRef);
   }
 }
