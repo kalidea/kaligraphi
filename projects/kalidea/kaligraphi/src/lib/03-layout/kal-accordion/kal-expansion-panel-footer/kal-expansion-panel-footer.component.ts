a308a5100b279ecb3eafb141b660f3d30b90220f
Corentin Davidenko <corentin.davidenko@up.coop>
2020-10-08 14:09:50 +0200

    Enhancement adding a kal-expansion-panel-footer allowing to have always visible content at the bottom of kal-expansion-panel in kal-accordion

---
+++ projects/kalidea/kaligraphi/src/lib/03-layout/kal-accordion/kal-expansion-panel-footer/kal-expansion-panel-footer.component.ts
@@ -0,0 +1,10 @@
+import { Component, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'kal-expansion-panel-footer',
+  templateUrl: './kal-expansion-panel-footer.component.html',
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class KalExpansionPanelFooterComponent {
+}
